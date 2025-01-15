6295965379eda0e1513b5381eddb6e17d5a0bb56
shadowik <zahar.mark@bk.ru>
2025-01-15 16:25:59 +0400

    Change structure. Add simple systemctl

--- initial_script.sh
+++ initial_script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-REPO="https://github.com/Playtronica/biotron-stand-firmware"
-DIR="biotron-stand-firmware"
+REPO_NAME="biotron-test-jig-software"
+REPO_SSH="git@github.com:Playtronica/biotron-test-jig-software.git"
 
-if [ ! -d "${DIR}" ]; then
-  git clone ${REPO}
+
+if [ ! -d "${REPO_NAME}" ]; then
+  git clone ${REPO_SSH}
 fi
 
-cd ${DIR} || exit
+cd ${REPO_NAME} || exit
 git pull
 
 pip install -r requirements.txt
---
+++ pre_initial_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt install git
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+cat  ~/.ssh/id_rsa.pub
+read -r -n 1 -p "Please enter this public ssh key to github repository. After press Enter" _
+
+REPO_NAME="biotron-test-jig-software"
+REPO_SSH="git@github.com:Playtronica/biotron-test-jig-software.git"
+
+git clone ${REPO_SSH}
+
+cp ~/${REPO_NAME}/initial_script.sh ~/
+cp ~/${REPO_NAME}/auto_update.service /etc/systemd/system
+
+systemctl daemon-reload
+systemctl enable auto_update.service
+systemctl start auto_update.service
