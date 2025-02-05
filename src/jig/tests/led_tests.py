fa62c8f44ae6b8b1ff64a2769bdfda8ab203338f
shadowik <zahar.mark@bk.ru>
2025-02-05 19:19:41 +0400

    Add delays to led test

--- src/jig/tests/led_tests.py
+++ src/jig/tests/led_tests.py
@@ -1,3 +1,4 @@
+import time
 from base_logger import get_logger_for_file
 from jig.jig_hardware_control.ads1015_4051 import MultiplexerADCReader
 
@@ -11,3 +12,4 @@ def led_tests():
         for multiplexer_channel_num in range(8):
             adc_val = adc_read.read_channel(multiplexer_num, multiplexer_channel_num)
             logger.info(f"Value of multiplexer {multiplexer_num} {multiplexer_channel_num}: {adc_val}")
+            time.sleep(0.1)
