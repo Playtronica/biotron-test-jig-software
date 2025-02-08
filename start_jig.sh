d943e5bfc1fe53028d2af15bdbfda340075dd93e
shadowik <zahar.mark@bk.ru>
2025-02-08 20:30:23 +0400

    Add pid

--- start_jig.sh
+++ start_jig.sh
@@ -6,11 +6,11 @@ PYTHON_SCRIPT="src/main.py"
 start_script() {
     echo "Starting Python script..."
     nohup sudo python "$PYTHON_SCRIPT"
-    echo $! > "$TARGET_DIR/script.pid"
+    echo $! > "script.pid"
 }
 
-if [ -f "$TARGET_DIR/script.pid" ]; then
-    PID=$(cat "$TARGET_DIR/script.pid")
+if [ -f "script.pid" ]; then
+    PID=$(cat "script.pid")
     if ! ps -p $PID > /dev/null; then
         echo "Python script is not running. Restarting..."
         start_script
